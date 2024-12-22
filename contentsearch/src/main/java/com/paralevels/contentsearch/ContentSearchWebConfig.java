6c8162ac66d04b94879e478db45798f259fc2a65
Felix Zheng <felix@paralevels.com>
2024-12-22 14:50:27 -0500

    Added ContentSearchWebConfig.java to package com.paralevels.contentsearch

--- contentsearch/src/main/java/com/paralevels/contentsearch/ContentSearchWebConfig.java
+++ contentsearch/src/main/java/com/paralevels/contentsearch/ContentSearchWebConfig.java
@@ -1,3 +1,5 @@
+package com.paralevels.contentsearch;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
