9dba843163fc30d1be0454bd8cbdcdd842ad57ab
Felix Zheng <felix@paralevels.com>
2024-12-24 15:14:09 -0500

    Refactored SolrService.java to implement search with cursor mark and remove the getResponse method

--- contentsearch/src/main/java/com/paralevels/contentsearch/solr/SolrService.java
+++ contentsearch/src/main/java/com/paralevels/contentsearch/solr/SolrService.java
@@ -14,47 +14,46 @@ public class SolrService {
     @Autowired
     private SolrClient solrClient;
 
-    private SolrQuery getQuery(String luceneQuery) {
+    private SolrQuery getQuery(String luceneQuery, String cursorMark) {
         SolrQuery query = new SolrQuery();
-        return query.setQuery(luceneQuery);
-    }
-
-    private List<SolrDocument> getResponse(SolrQuery query) throws Exception {
-        QueryResponse response = solrClient.query(query);
-        return response.getBeans(SolrDocument.class);
+        query.setQuery(luceneQuery);
+        query.set("cursorMark", cursorMark);
+        query.setRows(100);
+        query.setSort("id", SolrQuery.ORDER.asc);
+        return query;
     }
 
     /*
      * Base search function requires both field names and search keywords in
      * the searchTerm argument like (labels:keyword captions:keyword).
      */
-    public List<SolrDocument> search(String searchTerm) throws Exception {
-        SolrQuery query = this.getQuery(searchTerm);
-        return this.getResponse(query);
+    public QueryResponse search(String searchTerm, String cursorMark) throws Exception {
+        SolrQuery query = this.getQuery(searchTerm, cursorMark);
+        return solrClient.query(query);
     }
 
     /*
      * Search image labels only.
      */
-    public List<SolrDocument> searchLabels(String searchTerm) throws Exception {
-        SolrQuery query = this.getQuery("labels:" + searchTerm);
-        return this.getResponse(query);
+    public QueryResponse searchLabels(String searchTerm, String cursorMark) throws Exception {
+        SolrQuery query = this.getQuery("labels:" + searchTerm, cursorMark);
+        return solrClient.query(query);
     }
     
     /*
      * Search image captions only.
      */
-    public List<SolrDocument> searchCaptions(String searchTerm) throws Exception {
-        SolrQuery query = this.getQuery("captions:" + searchTerm);
-        return this.getResponse(query);
+    public QueryResponse searchCaptions(String searchTerm, String cursorMark) throws Exception {
+        SolrQuery query = this.getQuery("captions:" + searchTerm, cursorMark);
+        return solrClient.query(query);
     }
 
     /*
      * Search image tags only. Tags are words that are not related to
      * the objects in the image.
      */
-    public List<SolrDocument> searchTags(String searchTerm) throws Exception {
-        SolrQuery query = this.getQuery("tags:" + searchTerm);
-        return this.getResponse(query);
+    public QueryResponse searchTags(String searchTerm, String cursorMark) throws Exception {
+        SolrQuery query = this.getQuery("tags:" + searchTerm, cursorMark);
+        return solrClient.query(query);
     }
 }
